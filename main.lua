e002e8bb193e63205b78fac7cca9e29095558005
Daniel Moreira Pinto <danielmoreirapinto.ti@gmail.com>
2019-06-07 12:27:46 -0300

    Praticing via BYTEPATH tutorial

--- main.lua
+++ main.lua
@@ -1,21 +1,12 @@
-local canvas = love.graphics.newCanvas()
-local clear = false
-
-function love.keypressed(key)
-	if key == "c" then clear = true end
+function love.load()
+	archer = love.graphics.newImage('assets/images/archer.png')
 end
 
-function love.update()
-	canvas:renderTo(function()
-		if clear then love.graphics.clear() end
-		clear = false
-		love.graphics.setColor(love.math.random(255), 0, 0)
-		love.graphics.line(0, 0, love.math.random(0, love.graphics.getWidth()), love.math.random(0, love.graphics.getHeight()))
-	end)
+function love.update(dt)
+
 end
 
 function love.draw()
-	love.graphics.setColor(255, 255, 255)
-	love.graphics.draw(canvas)
+	love.graphics.draw(archer, love.math.random(0, 800), love.math.random(0, 800))
 end
 
