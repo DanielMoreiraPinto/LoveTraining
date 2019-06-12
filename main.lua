8c1a591ff4a768cb53c517eb462b8c6a7523ec3e
DanielMoreiraPinto <danielmoreirapinto.ti@gmail.com>
2019-06-11 22:50:28 -0300

    Pratice version

--- main.lua
+++ main.lua
@@ -1,5 +1,10 @@
+--[[
+Object = require 'libraries/classic'
+require 'objects/Test'
+
 function love.load()
-	archer = love.graphics.newImage('assets/images/archer.png')
+	--archer = love.graphics.newImage('assets/images/archer.png')
+	test_instance = Test()
 end
 
 function love.update(dt)
@@ -7,6 +12,18 @@ function love.update(dt)
 end
 
 function love.draw()
-	love.graphics.draw(archer, love.math.random(0, 800), love.math.random(0, 800))
+	--love.graphics.draw(archer, love.math.random(0, 800), love.math.random(0, 800))
 end
+--]]
 
+
+
+
+
+function love.draw()
+	love.graphics.setBackgroundColor(125, 150, 150)
+	love.graphics.setColor(1, 0.5, 0)
+	love.graphics.rectangle("fill", 200, 200, 85, 100)
+	love.graphics.setColor(0, 0, 1)
+	love.graphics.circle("line", 250, 250, 56)
+end
